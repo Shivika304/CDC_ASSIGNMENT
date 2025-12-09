91b8b84f544d0e7da2c762f98c0aca8156b29a69
Shivika304 <shivikasingh3004@gmail.com>
2025-12-09 18:21:38 +0530

    assignment 21-10-25 added

---
+++ 21-10-25/duplicateNum.java
@@ -0,0 +1,15 @@
+
+public class duplicateNum {
+    public int findDuplicate(int[] nums) {
+        int a[]=new int[nums.length];
+        for(int i:nums){
+            a[i]++;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(a[i]>1){
+                return i;
+            }
+        }
+        return 0;
+    }
+}
